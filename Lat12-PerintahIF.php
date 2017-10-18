e9816e7f88a59ad6780dbcfe510162522a617a0d
Ihda Mawadddah Nurmaulidya <ihdamawaddah@gmail.com>
2017-10-18 13:38:15 +0700

    Tambah file Lat12-Perintah.php

---
+++ Lat12-PerintahIF.php
@@ -0,0 +1,16 @@
+<html>
+
+    <head>
+        <title>Struktur Kendali IF</title>
+    </head>
+
+    <body>
+        <?php 
+            $x=13;
+            
+            if ($x < 15) 
+            { print ("Selamat Siang"); }
+        ?>
+    </body>
+
+</html>
