a003249c7f1ecfa0456b6671f78f5e1c94a9c41b
Ihda Mawadddah Nurmaulidya <ihdamawaddah@gmail.com>
2017-10-18 20:41:55 +0700

    edit file autoload.php

--- autoload.php
+++ autoload.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright 2014 Facebook, Inc.
- *
+ * ihdamawaddahnurmaulidya
  * You are hereby granted a non-exclusive, worldwide, royalty-free license to
  * use, copy, modify, and distribute this software in source code or binary
  * form for use in connection with the web services and APIs provided by
